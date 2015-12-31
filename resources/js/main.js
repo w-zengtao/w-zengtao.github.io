468129c263c10388d214c662156a00db17f8316e
ZengWeimao <so.zengtao@gmail.com>
2015-12-31 13:58:21 +0800

    js auto resize and todo is about me

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+  init();
+})
 
-});
+// 初始值设定
+function init() {
+  set_size();
+  re_size();
+}
 
-$(window).resize(function(){
-  var $win_width = $(window).width();
-  var $win_height = $(window).height();
+// 随着窗口的大小重设css尺寸
+function re_size() {
+  $(window).resize(function(){
+    set_size();
+  });
+}
 
-});
+function set_size() {
+  var win_width = $(window).width();
+  var win_height = $(window).height();
 
+  $(".screen-line").css("width",win_width);
+  $(".screen").css({
+    "width" : win_width,
+    "height" : win_height
+  });
+  $("#info-content").css({
+  });
+}
